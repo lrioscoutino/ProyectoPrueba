72a368235c04dace7a0b7125514fefe6467b5e19
Luis Rios Coutino <lrioscoutino@gmail.com>
2024-02-26 20:49:48 -0600

    ADD Create Student

--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import HttpResponse
 from students.models import Student
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -17,3 +18,14 @@ def students_list_view(request):
     students = Student.objects.all()
     context = {"students":students}
     return render(request, "students/list.html", context=context)
+
+
+def student_add_view(request):
+    if request.method == 'POST':
+        student = Student()
+        student.name = request.POST['name']
+        student.control_number = request.POST['control_number']
+        student.semester = request.POST['semester']
+        student.save()
+        return redirect(reverse_lazy('student-list'))
+    return render(request, "students/add.html")
